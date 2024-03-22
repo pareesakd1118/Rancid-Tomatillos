047f2cadc4494d581ff715c759fc6a9ebc4928f9
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-03-21 19:30:28 -0700

    Fetch data from API

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
 import Nav from "../Nav/Nav";
 import Main from "../Main/Main";
+import { useState, useEffect } from "react";
 import React from "react";
-import movieData from "../sample-data";
 
 function App() {
+  const [ movies, setMovies ] = useState([])
+
+  function getMovies() {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+    .then(res => {
+      if (!res.ok) {
+        console.log("error")
+      } else {
+        return res.json()
+      }
+    })
+    .then(data => setMovies(data.movies))
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
 
   return (
     <React.Fragment>
       <Nav />
       <Main 
-      movieData={movieData.movies}/>
+      movieData={movies}/>
     </React.Fragment>
   );
 }
-
+/*
+ <React.Fragment>
+        {!selectedMovie ? (
+      <Main 
+      movieData={movieData.movies}/>
+      ) : (
+        <DisplayDesc/> )}
+      </React.Fragment>
+*/
 export default App;
